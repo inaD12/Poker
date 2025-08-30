8e5a4ae97a5e48351517e00381238ccf05a816a8
inaD12 <capitanblack747@gmail.com>
2025-08-30 14:45:07 +0300

    Fix: Folding showdown bug fix

--- PokerServer/src/Modules/Game/Poker.Game.Domain/Entities/TableAggregate/Table.cs
+++ PokerServer/src/Modules/Game/Poker.Game.Domain/Entities/TableAggregate/Table.cs
@@ -453,6 +453,12 @@ public sealed class Table : Entity
         {
             var winner = activePlayers[0];
             winner.AddToBalance(CurrentPot);
+            
+            RaiseDomainEvent(new ShowdownDomainEvent(
+                Id,
+                [winner.Id],
+                CurrentPot,
+                GetPlayersStateDtosWithCards()));
             return;
         }
 
